63f12ec547b539cf92080379229d4253606c7a72
gg12368 <56186550+gg12368@users.noreply.github.com>
2019-11-18 22:03:50 +0800

    Create LazySingleton2.java
    多线程版--性能低

---
+++ LazySingleton2.java
@@ -0,0 +1,16 @@
+public class LazySingleton {
+    private static Singleton instance = null;
+    private Singleton() {
+
+    }
+    public synchronized static Singleton getInstance() {
+        if (instance == null) {
+                instance = new Singleton();
+        }
+        return instance;
+    }
+}
+问题：
+public synchronized static Singleton getInstance()保证了线程安全性；
+但是只有在第一次执行此方法时，才真正需要同步，
+所以一旦初始化完毕后，就每次调用这个方法，同步都是多余的。增加了开销。
